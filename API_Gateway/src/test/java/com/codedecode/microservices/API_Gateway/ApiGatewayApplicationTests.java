881cb4f670a0eb5939b5e2d177b227864d50491e
codedecode25 <codergaming25@gmail.com>
2021-12-28 23:22:06 +0530

    Adding API Gateway code

---
+++ API_Gateway/src/test/java/com/codedecode/microservices/API_Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedecode.microservices.API_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
